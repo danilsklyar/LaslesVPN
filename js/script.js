404df41193c97ed1f21d19750761fca82c50c120
danilsklyar <100023622+danilsklyar@users.noreply.github.com>
2022-06-15 18:08:06 +0300

    add Slider

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.comments__carousel').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow:3,
+        speed:1000,
+        centerMode:true,
+        
+    });
+});
