6d985fb93883211ed0c7c32271c543eaf052b7ad
danilsklyar <100023622+danilsklyar@users.noreply.github.com>
2022-06-18 20:43:25 +0300

    LaslesVPN(end)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ $(document).ready(function() {
         slidesToShow:3,
         speed:1000,
         centerMode:true,
-        
+        responsive: [
+            {
+                breakpoint: 1010,
+                settings: {
+                    slidesToShow: 2,
+                    arrows:false,
+                },
+            },
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 1,
+                    arrows:false,
+                }
+            }
+        ]
+    });
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list-active');
     });
 });
